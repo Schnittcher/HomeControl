aeca2a81dcb17ccfbf4fcc148b09778b0d84a259
Kai <kai.schnittcher@gmail.com>
2020-03-27 21:00:24 +0100

    Filter für ReceiveData hinzugefügt

--- HCDeviceAutomations/module.php
+++ HCDeviceAutomations/module.php
@@ -29,6 +29,7 @@ include_once __DIR__ . '/../libs/data.php';
         {
             //Never delete this line!
             parent::ApplyChanges();
+            $this->SetReceiveDataFilter('.*' .$this->ReadPropertyString('ModuleID'). '.*');
         }
 
         public function RequestAction($Ident, $Value)
